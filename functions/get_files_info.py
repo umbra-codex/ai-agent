36b2067998188c857fee4db05797b6152433cef1
UmbraCodex <the-shadow-skript@gmail.com>
2026-03-04 00:23:19 -0500

    03: added a function retrieve files metadata

---
+++ functions/get_files_info.py
@@ -0,0 +1,19 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+  working_dir_abs = os.path.abspath(working_directory)
+  target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+  
+  valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+  if not valid_target_dir:
+    return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+  
+  if not os.path.isdir(working_dir_abs):
+    return f'Error: "{directory}" is not a directory'
+  
+  lines = []
+
+  for item in os.listdir(target_dir):
+    lines.append(f"- {item}: file_size={os.path.getsize(os.path.join(target_dir, item))} bytes, is_dir={os.path.isdir(os.path.join(target_dir, item))}")
+
+  return "\n".join(lines) + "\n"
